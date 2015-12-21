9cca08fad738e78aab8eec75f8d5e0a635988983
alexanderceoh <alexanderceoh@gmail.com>
2015-12-21 18:31:18 -0500

    retrieved images from Parse

---
+++ InstagramClone/TableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  TableViewCell.swift
+//  InstagramClone
+//
+//  Created by alex oh on 12/21/15.
+//  Copyright © 2015 Alex Oh. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    @IBOutlet weak var postedImage: UIImageView!
+    
+    @IBOutlet weak var username: UILabel!
+    
+    @IBOutlet weak var message: UILabel!
+    
+}
